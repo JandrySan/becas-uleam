9796f8df5efd6b381e2976b138ef458942cbf568
remanCO Skill Issue <alavajg@gmail.com>
2026-07-09 22:38:59 -0500

    Eliminar archivos de enrutamiento y componente de inicio de sesión; agregar configuración de entorno para Supabase

--- 3seguimiento-angular/src/app/services/supabase.service.ts
+++ 3seguimiento-angular/src/app/services/supabase.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://wapndygnusbxhkraijcr.supabase.co';
-const supabaseKey = 'sb_publishable_Xql8RBTLvwc9I1Fwbry3WQ_ah2a6rSX';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SupabaseService {
-  client = createClient(supabaseUrl, supabaseKey);
+  client = createClient(environment.supabaseUrl, environment.supabaseKey);
 }
